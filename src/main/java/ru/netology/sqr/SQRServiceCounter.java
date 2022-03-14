a97b762fb2b50d890bde50294b7879b54c1bda45
irina godonoage <godonoage@bk.ru>
2022-03-14 17:16:39 +0300

    изменения

--- src/main/java/ru/netology/sqr/SQRServiceCounter.java
+++ src/main/java/ru/netology/sqr/SQRServiceCounter.java
@@ -1,14 +1,13 @@
 package ru.netology.sqr;
 
 public class SQRServiceCounter {
-    public int counter(int startnum, int endnum) {
-        int j = 0;
+    public int numbersCounter(int startNum, int endNum) {
+        int counter = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= startnum & i * i <= endnum) j++;
-            ///if (i * i >= startnum & i * i <= endnum)
-                //System.out.println("Цифры квадрат которых попал в диапазон - " + i);
+            if (i * i >= startNum && i * i <= endNum) counter++;
+
         }
 
-        return j;
+        return counter;
     }
 }
--- src/test/java/ru/netology/sqr/SQRServiceCounterTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceCounterTest.java
@@ -1,7 +1,5 @@
 package ru.netology.sqr;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceCounterTest {
@@ -9,49 +7,53 @@ class SQRServiceCounterTest {
     @org.junit.jupiter.api.Test
     void counterLeftBorder() {
         SQRServiceCounter service = new SQRServiceCounter();
-        int startnum = 95;
-        int endnum = 99;
+        int startNum = 95;
+        int endNum = 99;
         int expected = 0;
-        int actual = service.counter(startnum,endnum);
-        assertEquals (expected,actual);
+        int actual = service.numbersCounter(startNum, endNum);
+        assertEquals(expected, actual);
     }
+
     @org.junit.jupiter.api.Test
-    void counterRightBorder(){
+    void counterRightBorder() {
         SQRServiceCounter service = new SQRServiceCounter();
-        int startnum = 9802;
-        int endnum = 10000;
+        int startNum = 9802;
+        int endNum = 10000;
         int expected = 0;
-        int actual = service.counter(startnum,endnum);
-        assertEquals (expected,actual);
+        int actual = service.numbersCounter(startNum, endNum);
+        assertEquals(expected, actual);
 
     }
+
     @org.junit.jupiter.api.Test
-    void counterWorkBorder(){
+    void counterWorkBorder() {
         SQRServiceCounter service = new SQRServiceCounter();
-        int startnum = 200;
-        int endnum = 600;
+        int startNum = 200;
+        int endNum = 600;
         int expected = 10;
-        int actual = service.counter(startnum,endnum);
-        assertEquals (expected,actual);
+        int actual = service.numbersCounter(startNum, endNum);
+        assertEquals(expected, actual);
 
     }
+
     @org.junit.jupiter.api.Test
-    void counterBorder(){
+    void counterBorder() {
         SQRServiceCounter service = new SQRServiceCounter();
-        int startnum = 100;
-        int endnum = 9801;
+        int startNum = 100;
+        int endNum = 9801;
         int expected = 90;
-        int actual = service.counter(startnum,endnum);
-        assertEquals (expected,actual);
+        int actual = service.numbersCounter(startNum, endNum);
+        assertEquals(expected, actual);
 
     }
+
     @org.junit.jupiter.api.Test
     void counterWorkBorder2() {
         SQRServiceCounter service = new SQRServiceCounter();
-        int startnum = 500;
-        int endnum = 5000;
+        int startNum = 500;
+        int endNum = 5000;
         int expected = 48;
-        int actual = service.counter(startnum, endnum);
+        int actual = service.numbersCounter(startNum, endNum);
         assertEquals(expected, actual);
     }
 
